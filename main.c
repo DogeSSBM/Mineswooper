2042da94ae6b211a8c84eee4e6dfa6839a773f59
dogeThinkpad <dogessbm@gmail.com>
2022-05-23 14:04:21 -0400

    almost done

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ int imax(const int a, const int b)
 uint scale(const Length len)
 {
     const Length win = getWindowLen();
-    return imin(win.x/len.x, win.x/len.x);
+    return imin(win.x/len.x, win.y/len.y);
 }
 
 void resetTiles(Tile **tile, const Length len)
@@ -93,20 +93,51 @@ void getNums(Tile **tile, const Length len)
     }
 }
 
-Board boardInit(const Length len, const Coord firstClick, const uint numBombs)
+uint numTilesLeft(Tile **tile, const Length len, const uint numBombs)
+{
+    if(!tile)
+        return 0;
+    uint left = len.x*len.y - numBombs;
+    for(int y = 0; y < len.y; y++)
+        for(int x = 0; x < len.x; x++)
+            left -= !tile[x][y].isBomb && tile[x][y].clear;
+
+    return left;
+}
+
+Board boardFree(Board board)
+{
+    if(board.tile){
+        for(uint x = 0; x < board.len.x; x++)
+            free(board.tile[x]);
+        free(board.tile);
+        board.tile = NULL;
+    }
+    return board;
+}
+
+Board boardInit(const Length len)
 {
     Board ret = {
         .len = len,
         .scale = scale(len),
-        .numBombs = numBombs,
+        .numBombs = 0,
         .tile = calloc(len.x, sizeof(Tile*))
     };
     for(uint x = 0; x < len.x; x++)
         ret.tile[x] = calloc(len.y, sizeof(Tile));
-
-    placeBombs(ret.tile, len, firstClick, numBombs);
-    getNums(ret.tile, len);
     return ret;
+}
+
+Board boardFirstClick(Board board, const Coord firstClick, const uint numBombs)
+{
+    board = boardFree(board);
+    board = boardInit(board.len);
+    board.numBombs = numBombs;
+    placeBombs(board.tile, board.len, firstClick, numBombs);
+    board.tilesLeft = numTilesLeft(board.tile, board.len, numBombs);
+    getNums(board.tile, board.len);
+    return board;
 }
 
 uint parseUint(char *str)
@@ -122,6 +153,17 @@ uint parseUint(char *str)
 
 void drawBoard(const Board board)
 {
+    const static Color numColor[] = {
+        {0x00, 0x00, 0xAA, 0xFF},
+        {0x00, 0x52, 0x00, 0xFF},
+        {0xAA, 0x00, 0x00, 0xFF},
+        {0x00, 0x00, 0x52, 0xFF},
+        {0x52, 0x00, 0x00, 0xFF},
+        {0x00, 0x52, 0x52, 0xFF},
+        {0x00, 0x00, 0x00, 0xFF},
+        {0x52, 0x52, 0x52, 0xFF},
+    };
+
     for(uint y = 0; y < board.len.y; y++){
         for(uint x = 0; x < board.len.x; x++){
             const Coord pos = coordMul((const Coord){.x = x, .y = y}, board.scale);
@@ -130,6 +172,16 @@ void drawBoard(const Board board)
             if(board.tile[x][y].clear){
                 setColor(GREY3);
                 fillSquareCoordResize(pos, board.scale, -1);
+                if(board.tile[x][y].num > 0 && board.tile[x][y].num < 9){
+                    setTextSize(board.scale);
+                    setTextColor(numColor[board.tile[x][y].num-1]);
+                    char txt[2] = "0";
+                    txt[0] = '0'+board.tile[x][y].num;
+                    drawTextCenteredCoord(
+                        txt,
+                        coordOffset(pos, (const Coord){.x=board.scale/2,.y=board.scale/2})
+                    );
+                }
             }else{
                 setColor(GREY1);
                 fillSquareCoordResize(pos, board.scale, -1);
@@ -151,6 +203,59 @@ void printBoard(const Board board)
         putchar('\n');
     }
     putchar('\n');
+}
+
+Board prop(Board board, const Coord pos)
+{
+    if(!validTilePos(pos, board.len) || board.tile[pos.x][pos.y].isBomb)
+        return board;
+    board.tile[pos.x][pos.y].clear = true;
+    if(board.tile[pos.x][pos.y].num)
+        return board;
+    for(Direction d = 0; d < 4; d++){
+        const Coord adj = coordShift(pos, d, 1);
+        if(validTilePos(adj, board.len)){
+            board = prop(board, adj);
+        }
+        const Coord dag = coordShift(dag, dirROR(d), 1);
+        if(validTilePos(dag, board.len)){
+            board = prop(board, dag);
+        }
+    }
+    return board;
+}
+
+void loose(Board board, const Coord bombClicked)
+{
+    Length window = getWindowLen();
+    while(1){
+        Ticks t = frameStart();
+
+        const Length newWindow = getWindowLen();
+        if(!coordSame(newWindow, window)){
+            board.scale = scale(board.len);
+        }
+
+        drawBoard(board);
+        const Coord pos = coordMul((const Coord){.x=bombClicked.x, .y=bombClicked.y}, board.scale);
+        setColor((const Color){.r = 255, .g = 128, .b = 0, .a = 255});
+        fillSquareCoordResize(pos, board.scale, -1);
+
+        setTextSize(board.scale);
+        setColor(WHITE);
+        const Length tlen = getTextLength(" You loose! ");
+        fillRectCoordLength(coordOffset(getWindowMid(), coordDiv(tlen, -2)), tlen);
+        setColor(BLACK);
+        fillBorderCoords(coordOffset(getWindowMid(), coordDiv(tlen, -2)), tlen, board.scale/4);
+
+        setTextColor(RED);
+        drawTextCenteredCoord(" You loose! ", getWindowMid());
+        if(keyPressed(SDL_SCANCODE_ESCAPE)){
+            boardFree(board);
+            exit(0);
+        }
+        frameEnd(t);
+    }
 }
 
 int main(int argc, char **argv)
@@ -180,24 +285,43 @@ int main(int argc, char **argv)
     setWindowLen(window);
 
     bool firstClick = true;
-    Board board = {0};
-    board.scale = scale(len);
+    Board board = boardInit(len);
     while(1){
         Ticks t = frameStart();
 
         if(mouseBtnPressed(MOUSE_L)){
             const Coord tilePos = coordDiv(mouse.pos, board.scale);
-            if(firstClick){
-                if(validTilePos(tilePos, len)){
-                    board = boardInit(len, tilePos, bombs);
-                    printBoard(board);
+            printf("Lm:(%2i,%2i)[%2i,%2i]\n", mouse.pos.x, mouse.pos.y, tilePos.x, tilePos.y);
+            if(validTilePos(tilePos, board.len)){
+                if(firstClick){
+                    board = boardFirstClick(board, tilePos, bombs);
+                    // printBoard(board);
+                    firstClick = false;
+                    board = prop(board, tilePos);
+                }else{
+                    if(board.tile[tilePos.x][tilePos.y].isBomb)
+                        loose(board, tilePos);
+                    if(!board.tile[tilePos.x][tilePos.y].clear)
+                        board = prop(board, tilePos);
                 }
-            }else{
-
             }
         }
         if(mouseBtnPressed(MOUSE_R)){
+            const Coord tilePos = coordDiv(mouse.pos, board.scale);
+            if(validTilePos(tilePos, board.len)){
+                board.tile[tilePos.x][tilePos.y].decal++;
+                board.tile[tilePos.x][tilePos.y].decal %= D_N;
+            }
+        }
 
+        const Length newWindow = getWindowLen();
+        if(!coordSame(newWindow, window)){
+            board.scale = scale(board.len);
+        }
+
+        if(keyPressed(SDL_SCANCODE_ESCAPE)){
+            boardFree(board);
+            return 0;
         }
 
         drawBoard(board);
