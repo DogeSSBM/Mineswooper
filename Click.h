040c1ff5c7835470a978b0726e621f58408e8494
dogeThinkpad <dogessbm@gmail.com>
2022-08-24 14:41:45 -0400

    Automatic commit.

--- Click.h
+++ Click.h
@@ -14,13 +14,27 @@ Coord tileMousePos(const Board board, const Coord tpos)
 ClickType clickInput(const Board board)
 {
     static Coord down[2] = {0};
-    if(mouseBtnPressed(MOUSE_L))
-        down[0] = coordDiv(mouse.pos, board.scale);
-    if(mouseBtnPressed(MOUSE_R))
-        down[1] = coordDiv(mouse.pos, board.scale);
 
-    (void)down;
-    return C_FLAG;
+    if(mouseBtnPressed(MOUSE_L))
+        down[0] = mouseTilePos(board);
+    if(mouseBtnPressed(MOUSE_R))
+        down[1] = mouseTilePos(board);
+
+    if(
+        mouseBtnReleased(MOUSE_L) &&
+        coordSame(down[0], mouseTilePos(board)) &&
+        !mouseBtnState(MOUSE_R)
+    )
+        return C_CLEAR;
+
+    if(
+        mouseBtnReleased(MOUSE_R) &&
+        coordSame(down[1], mouseTilePos(board)) &&
+        !mouseBtnState(MOUSE_L)
+    )
+        return C_FLAG;
+
+    return C_NONE;
 }
 
 #endif /* end of include guard: CLICK_H */
