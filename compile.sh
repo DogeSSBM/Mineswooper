ac16ffaa457075c1671d57bc2decde97e566ec49
DogeSSBM <dogessbm@gmail.com>
2022-09-15 02:10:49 -0400

    Automatic commit.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 clear
 libraries="-lm -lSDL2 -lSDL2_ttf -lSDL2_gfx -lSDL2_image"
-flags="-Wall -Wextra -Wpedantic -g"
+flags="-Wall -Wextra -Wpedantic -Og -g3"
 gcc main.c -std=c11 $flags $libraries -o main.out
